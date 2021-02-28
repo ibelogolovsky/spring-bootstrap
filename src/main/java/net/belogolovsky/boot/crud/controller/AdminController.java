f6e27d838a680f5b80c1cc3e4395719fae1b7204
Igor Belogolovsky <igor@belogolovsky.net>
2021-02-28 08:48:45 +0300

    Initial commit

---
+++ src/main/java/net/belogolovsky/boot/crud/controller/AdminController.java
@@ -0,0 +1,87 @@
+package net.belogolovsky.boot.crud.controller;
+
+import net.belogolovsky.boot.crud.model.Role;
+import net.belogolovsky.boot.crud.model.User;
+import net.belogolovsky.boot.crud.service.RoleService;
+import net.belogolovsky.boot.crud.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RoleService roleService;
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("users", userService.listAll());
+        return "admin/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") long id, Model model) {
+        model.addAttribute("user", userService.get(id));
+        return "admin/show";
+    }
+
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user") User user,
+                          Model model) {
+        model.addAttribute("allRoles", roleService.listAll());
+        return "admin/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("user") User user,
+                         @RequestParam(value = "roles") Role[] rolesArray) {
+        User newUser = new User(user);
+        for (Role role : rolesArray) {
+            userService.addRole(newUser, role.getName());
+        }
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") long id, Model model) {
+        model.addAttribute("user", userService.get(id));
+        model.addAttribute("allRoles", roleService.listAll());
+        return "admin/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("user") User user,
+                         @RequestParam(value = "roles", required = false) Role[] rolesArray,
+                         @PathVariable("id") long id) {
+        user.setId(id);
+        User dbUser = userService.get(id);
+//        user.setLogin(dbUser.getLogin());
+        boolean passwordUpdated = true;
+        if (user.getPassword().equals("")) {
+            user.setPassword(dbUser.getPassword());
+            passwordUpdated = false;
+        }
+        user.setRoles(new HashSet<>());
+        userService.save(user, passwordUpdated);
+        if (rolesArray != null) {
+            for (Role role : rolesArray) {
+                userService.addRole(user, role.getName());
+            }
+        }
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") long id) {
+        userService.remove(id);
+        return "redirect:/admin";
+    }
+}
---
+++ src/main/java/net/belogolovsky/boot/crud/service/UserServiceImpl.java
@@ -0,0 +1,97 @@
+package net.belogolovsky.boot.crud.service;
+
+import net.belogolovsky.boot.crud.dao.RoleRepository;
+import net.belogolovsky.boot.crud.dao.UserRepository;
+import net.belogolovsky.boot.crud.model.Role;
+import net.belogolovsky.boot.crud.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository users;
+
+    @Autowired
+    private RoleRepository roles;
+
+    private PasswordEncoder encoder;
+
+    @Autowired
+    public void setPasswordEncoder(PasswordEncoder encoder) {
+        this.encoder = encoder;
+    }
+
+    public UserServiceImpl() {}
+
+    @Override
+    public void save(User user) {
+        save(user, users.findByEmail(user.getEmail()).isEmpty());
+    }
+
+    @Override
+    public void save(User user, boolean encodePassword) {
+        if (encodePassword) {
+            user.setPassword(encoder.encode(user.getPassword()));
+        }
+        users.save(user);
+    }
+
+    @Override
+    public void setPassword(User user, String password) {
+        user.setPassword(encoder.encode(password));
+    }
+
+    @Override
+    public User get(long id) {
+        return users.findById(id).orElseGet(User::new);
+    }
+
+    @Override
+    public void remove(long id) {
+        users.deleteById(id);
+    }
+
+    @Override
+    public List<User> listAll() {
+        return (List<User>) users.findAll();
+    }
+
+    @Override
+    public Optional<User> findByLogin(String email) {
+        return users.findByEmail(email);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return findByLogin(login).orElseThrow(
+                () -> new UsernameNotFoundException(String.format("User %s not found", login))
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> rolesToAuthorities(Collection<Role> roles) {
+        return roles.stream()
+                .map(
+                        role -> new SimpleGrantedAuthority(role.getName())
+                ).collect(Collectors.toList());
+    }
+
+    @Override
+    public void addRole(User user, String roleName) {
+        Role role = roles.findByName(roleName).orElseGet(() -> new Role(roleName));
+        user.getRoles().add(role);
+        save(user);
+    }
+}
